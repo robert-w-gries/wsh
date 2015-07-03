88e4c3e634ac11f76a73b7bd36501aeb1313a1fb
Rob Gries <robert.w.gries@gmail.com>
2015-07-03 17:50:37 -0500

    Re-implemented platform code to comply with platform header

--- platform/wsh_posix.c
+++ platform/wsh_posix.c
@@ -1,26 +1,31 @@
+#include <dirent.h>
+#include <errno.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/param.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include "platform.h"
 #include "../include/wsh_status.h"
 
-static enum WSH_STATUS posix_change_directory(wsh_command *cmd);
-static int posix_create_process(wsh_command *cmd);
-static void posix_signal_handler();
 static void SignalHandler(int signal);
 
 void init_platform(platform *p) {
 
     p->name = "Posix";
-    p->change_directory = &posix_change_directory;
-    p->create_process = &posix_create_process;
-    p->signal_handler = &posix_signal_handler;
+
+    p->cwd = (char *)malloc(MAXPATHLEN * sizeof(char));
+    if (NULL == p->cwd) {
+        fprintf(stderr, "Error: Out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    get_current_directory();
 
 }
 
-static enum WSH_STATUS posix_change_directory(wsh_command *cmd) {
+enum WSH_STATUS change_directory(wsh_command *cmd) {
 
     char *path = cmd->args2D[1];
 
@@ -38,7 +43,7 @@ static enum WSH_STATUS posix_change_directory(wsh_command *cmd) {
 
 }
 
-static int posix_create_process(wsh_command *cmd) {
+enum WSH_STATUS create_process(wsh_command *cmd) {
 
     int status = 0;
 
@@ -57,17 +62,58 @@ static int posix_create_process(wsh_command *cmd) {
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 
-    return 0;
+    return OK;
 }
 
-static void posix_signal_handler() {
+char * get_current_directory() {
+
+    int buf_size = MAXPATHLEN;
+    while (NULL == getcwd(my_platform.cwd, buf_size)) {
+
+        if (EINVAL == errno) {
+            buf_size += MAXPATHLEN;
+            my_platform.cwd = (char *)realloc(my_platform.cwd, buf_size * sizeof(char));
+            continue;
+        }
+
+        perror("wsh");
+        exit(EXIT_FAILURE);
+
+    }
+
+    return my_platform.cwd;
+
+}
+
+enum WSH_STATUS list_directory_contents(wsh_command *cmd) {
+
+    struct dirent *dir;
+    DIR *ls_dir = (1 == cmd->nargs) ? opendir(".") : opendir(cmd->args2D[1]);
+
+    if (NULL == ls_dir) {
+        perror("wsh");
+        exit(EXIT_FAILURE);
+    }
+
+    while (NULL != (dir = readdir(ls_dir))) {
+        printf("%s\n", dir->d_name);
+    }
+
+    closedir(ls_dir);
+    return OK;
+
+}
+
+enum WSH_STATUS signal_handler() {
     signal(SIGINT, SignalHandler);
+    signal(SIGABRT, SignalHandler);
+    return OK;
 }
 
 static void SignalHandler(int signal) {
 
     if (SIGINT == signal) {
-        printf("Ctrl+C detected\n");
+        printf("\n");
         exit(EXIT_FAILURE);
     } else if (SIGABRT == signal) {
         exit(EXIT_FAILURE);
